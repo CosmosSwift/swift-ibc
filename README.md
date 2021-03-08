ed8378e3d1968efd802b8d4be752f4637b532300
Alex Tran Qui <alex@katalysis.io>
2021-03-08 17:13:02 +0100

    Initial checkin

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-ibc
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swift_ibcTests
+
+var tests = [XCTestCaseEntry]()
+tests += swift_ibcTests.allTests()
+XCTMain(tests)
